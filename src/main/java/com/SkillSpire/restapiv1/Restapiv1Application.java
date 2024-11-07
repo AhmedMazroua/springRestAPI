c699f7b0b066aacf6c7241b661f3be90dbddb2df
AhmedMazroua <126991045+AhmedMazroua@users.noreply.github.com>
2024-11-06 22:03:55 -0800

    version2

--- src/main/java/com/SkillSpire/restapiv1/Restapiv1Application.java
+++ src/main/java/com/SkillSpire/restapiv1/Restapiv1Application.java
@@ -3,6 +3,7 @@ package com.SkillSpire.restapiv1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Restapiv1Application {
 
